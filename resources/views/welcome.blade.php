599f4dfb8e7a918f5718c10cd4064a3a52e794de
noproname1998 <noproname1998@gmail.com>
2018-08-19 12:54:29 +0700

    laravelchat init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Chat</title>
+</head>
+<body>
+    <div id="app">
+        <example-component></example-component>
+    </div>
+    <script src="js/app.js"></script>
+</body>
+</html>
